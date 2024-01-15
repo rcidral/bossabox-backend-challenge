b2eb46457a024c9b868630b104923f9fc231d22e
rcidral <ricidralmfo@gmail.com>
2024-01-14 22:52:25 -0300

    feat: update database

--- config/db.sql
+++ config/db.sql
@@ -1,6 +1,6 @@
 -- create tools table --
 CREATE TABLE tools (
-    id INT PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     link VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
