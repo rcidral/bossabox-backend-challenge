e01ee01a87ba2902e165771db0e9492bcb105584
rcidral <ricidralmfo@gmail.com>
2024-01-15 17:26:10 -0300

    feat: set docker config

--- src/constants/envDbConstants.ts
+++ src/constants/envDbConstants.ts
@@ -1,6 +1,9 @@
 import * as dotenv from 'dotenv'
+import { env } from '../infra/env'
+
 dotenv.config()
 
 export default class envDbConstants {
-  static url: string | any = process.env.DATABASE_URL
+  static url: string | any =
+    env.NODE_ENV === 'production' ? env.DATABASE_URL : env.LOCAL_DATABASE_URL
 }
